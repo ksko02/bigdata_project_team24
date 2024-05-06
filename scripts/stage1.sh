2b3f29eb8f05569bc81984f594fa63a7df7b84bc
ksko02 <o.konovalova@innopolis.university>
2024-05-07 00:49:00 +0300

    add stage1

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# Clean old data
+rm data/*
+rm output/*
+
+# Download dataset
+url="https://disk.yandex.ru/d/xYqRP_WhIbowPw"
+
+wget "$(yadisk-direct $url)" -O data/metaverse_transactions_dataset.csv
+
+# Build PostgreSQL database
+python3 scripts/build_projectdb.py
+
+password=$(head -n 1 secrets/.psql.pass)
+
+# Import data to HDFS via SQOOP 
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team24_projectdb --username team24 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1 --outdir output
