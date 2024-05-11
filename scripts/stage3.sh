9be77fba1dd0d9788ebc9c2ff34a2b789628a1b4
ksko02 <o.konovalova@innopolis.university>
2024-05-12 01:24:50 +0300

    Update stage3.sh

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-#Clean old data
-hdfs dfs -rm -r -skipTrash /user/team24/project/data
-hdfs dfs -rm -r -skipTrash /user/team24/project/output/*_predictions.csv
-hdfs dfs -rm -r -skipTrash /user/team24/project/output/evaluation.csv
-hdfs dfs -rm -r -skipTrash /user/team24/project/models
-
-rm models/*
-rm data/*.json
-rm output/*_predictions.csv
-rm output/evaluation.csv
-
 spark-submit --master yarn scripts/model.py
